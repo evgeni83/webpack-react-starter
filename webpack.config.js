728cdba8f69c5ec2cfb184b25c0fe1ef5ff89928
evgeni83 <48216627+evgeni83@users.noreply.github.com>
2021-06-25 13:57:13 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,59 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js',
-        clean: true,
-    },
-    devServer: {
-        port: 3333,
-        open: true,
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
+	resolve: {
+		extensions: [ '.js', '.jsx' ],
+	},
+	entry: path.resolve( __dirname, 'src', 'index.js' ),
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+		filename: 'js/bundle.min.js',
+		clean: true,
+		publicPath: '/'
+	},
+	devServer: {
+		port: 3333,
+		open: true,
+		hot: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
                 exclude: '/node_modules/',
                 use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				loader: 'file-loader',
+				options: {
+					outputPath: 'img',
+				},
+			},
+		]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        }),
-    ],
+		new HtmlWebpackPlugin( {
+			template: path.resolve( __dirname, 'src', 'index.html' )
+		} ),
+		new MiniCssExtractPlugin( {
+			filename: 'css/style.css',
+		} ),
+	],
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin({
